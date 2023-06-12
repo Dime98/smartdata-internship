b5e795ee5147ac5003dd3d74c0ac3f638e996ac0
melniciucd <melniciucd@gmail.com>
2023-06-12 17:37:08 +0300

    added simple overload

---
+++ task1_overload.py
@@ -0,0 +1,16 @@
+class A:
+    def __init__(self):
+        pass
+
+    def overload_method(self, arg1):
+        print("class A")
+
+class B:
+    def overload_method(self, arg1, arg2, arg3):
+        print("class B")
+
+a = A()
+a.overload_method(1)
+b = B()
+b.overload_method(1,2,3)
+
