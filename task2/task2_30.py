3a1bdc7518b5a053ad794cbc56a929f03c7b5310
melniciucd <melniciucd@gmail.com>
2023-06-14 14:45:35 +0300

    completed 14, 16, 20

--- task2/task2_30.py
+++ task2/task2_30.py
@@ -19,9 +19,15 @@ for i in np_arr:
 
 print()
 # print(np_arr[:,2])
-n, m = N-1, M-1
+# n, m = N-1, M-1
+n, m = 0, 0
+move_vert, move_horis = 0, 0
 for i in range(M):
-    while n != N:
-        print(np_arr[n,0])
-        n += 1
-    break
+    for x in range(10):
+        if x %2 == 0:
+            move_vert = 0
+            move_horis = 0
+        while move != N:
+            print(np_arr[0,move])
+            move += 1
+    
--- task3/task3_20.py
+++ task3/task3_20.py
@@ -7,16 +7,19 @@ df["Min.Price"].value_counts
 
 def repl_nan(val, replace_with):
     # print()
-    # print(f"val 	{val} {type(val)}")
-    # print(f"np.nan  {np.nan} {type(np.nan)}")
-    # print(np.isnan(val), val == np.nan)
-    # print(f"replace_with {replace_with}")
-    print(val)
-    if np.isnan(val):
+    print(f"val   {val} {type(val)}")
+    # # print(f"np.nan  {np.nan} {type(np.nan)}")
+    # # print(np.isnan(val), val == np.nan)
+    # # print(f"replace_with {replace_with}")
+    if np.isnan(val) or val == None:
         print("\t",val,replace_with)
-        return replace_with
+        val = replace_with
+    return val
 
 mean = df['Min.Price'].mean()
 df['Min.Price'] = df['Min.Price'].apply(lambda x : repl_nan(x, mean))
 # df['Min.Price'].mean()
-print(df['Min.Price'].value_counts)
+
+print()
+# print(df['Min.Price'])
+# print(df['Min.Price'].value_counts)
