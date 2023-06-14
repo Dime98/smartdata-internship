a245ce94a1e5b0d32b9d979a87eedb6f3588e075
melniciucd <melniciucd@gmail.com>
2023-06-14 15:42:39 +0300

    added 18 for task2

--- task2/task2_18.py
+++ task2/task2_18.py
@@ -1,6 +1,17 @@
-18. Given an unlimited supply of coins of given denominations, find the minimum number of 
-coins required to get the desired change.
+# 18. Given an unlimited supply of coins of given denominations, find the minimum number of 
+# coins required to get the desired change.
 
-denominations = [1, 5, 10, 50]
+def get_keys_by_value(dict_obj, value):
+    return [k for k, v in dict_obj.items() if v == value]
+
+denominations = [.01, .05, .10, .50]
 
 change = 20
+
+min_nr = {}
+for i in denominations:
+	min_nr[i] = int(change/i)
+
+shortest_len = min(min_nr.values())
+print(f"{get_keys_by_value(min_nr, shortest_len)[0]} minimum number of \
+coins required to get {change} change ")
