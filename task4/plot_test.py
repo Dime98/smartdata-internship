8869443c5df07578d2711200aaedd9a46c754546
melniciucd <melniciucd@gmail.com>
2023-06-15 14:15:00 +0300

    updated to latest

---
+++ task4/plot_test.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+fruits = ['apple', 'blueberry', 'cherry', 'orange']
+counts = [40, 100, 30, 55]
+# counts2 = [30]
+bar_labels = ['red', 'blue', '_red', 'orange']
+bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+ax.bar(fruits, counts)
+# ax.bar(fruits, counts, counts2, label=bar_labels, color=bar_colors)
+ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+ax.set_ylabel('fruit supply')
+ax.set_title('Fruit supply by kind and color')
+ax.legend(title='Fruit color')
+
+plt.show()
