91bc8b00e4b1cca6ae9d1f0b4dc00651ff7dead4
melniciucd <melniciucd@gmail.com>
2023-06-16 16:25:39 +0300

    added 2_21 and 2_17, 2_17 gives error but return is correct, can't stop recursion

--- task2/task2_17.py
+++ task2/task2_17.py
@@ -21,3 +21,61 @@ water_trapper = {
 
 water_trapper = [7, 0, 4, 2, 5, 0, 6, 4, 0, 5,]
 print(water_trapper)
+
+front_edge_bar = water_trapper[0]
+end_edge_bar = water_trapper[-1]
+print(f"front_edge_bar  {front_edge_bar}")
+print(f"end_edge_bar    {end_edge_bar}")
+
+start, end, direction = None, None, 1
+height = None
+if end_edge_bar < front_edge_bar:
+    start = len(water_trapper) - 1
+    end = 0
+    direction = -1
+    height = end_edge_bar
+else:
+    end = len(water_trapper) - 1
+    start = 0
+    height = front_edge_bar
+
+
+print(start, end, direction)
+print(f"height {height} ")
+store = []
+end_bool = False
+def guts(height, start, end, direction, water_trapper):
+    for index in range(start, end-1, direction):
+        print()
+        print(index, start, end)
+        print(f"[{index}] {water_trapper[index]}")
+        print(f"sum {sum(store)} {store}")
+        print(f"height {height}")
+
+        if index == end:
+            print(f"\t\t\tsum {sum(store)} {store}")
+            print("\t\t\t===================")
+        # if index != start or index != end:
+        while water_trapper[index] < height:
+            print(f"\t  [{index}] {water_trapper[index]} {end}")
+            store.append(height-water_trapper[index])
+            index += direction
+            print(f"\t  index {index} end {end} {index == end} {sum(store)}")
+        if index == end:
+            print(f"\t\t\t gotta stop here {sum(store)}")
+            end_bool = True
+            break
+        print(f"\t  [{index}] {water_trapper[index]}")
+        guts(water_trapper[index], index - 1, end, direction, water_trapper)
+        
+        if end_bool == True:
+            break
+
+        
+        # if start == end:
+            # break
+
+
+guts(height, start+direction, end, direction, water_trapper)
+print()
+print(store,sum(store))
--- task2/task2_21.py
+++ task2/task2_21.py
@@ -5,4 +5,16 @@ import os
 str_arr = ["123a", "123ddsa", '123abcaa', "12345"]
  
 prefix = os.path.commonprefix(str_arr)
-print(prefix)
+# print(prefix)
+
+def commonprefix(m):
+    if not m: return ''
+    m = tuple(map(os.fspath, m))
+    s1 = min(m)
+    s2 = max(m)
+    for i, c in enumerate(s1):
+        if c != s2[i]:
+            return s1[:i]
+    return s1
+
+print(commonprefix(str_arr))
